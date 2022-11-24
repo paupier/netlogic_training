25df49cce379d5edfd009d08ac50ff8450bcbd2b
paupier <ppierce2@ra.rockwell.com>
2022-11-24 13:48:33 +0100

    working designtime script

---
+++ ProjectFiles/NetSolution/DesignTimeNetLogic1.cs
@@ -0,0 +1,36 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.UI;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.Core;
+using FTOptix.CoreBase;
+using FTOptix.NetLogic;
+#endregion
+
+public class DesignTimeNetLogic1 : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+        var myFolder = Project.Current.Get<Folder>("Model/Variables");
+        if (myFolder != null) { 
+            myFolder.Delete(); 
+        }
+        var myNewFolder = InformationModel.Make<Folder>("Variables");
+        var myModelFolder = Project.Current.Get<Folder>("Model");
+        myModelFolder.Add(myNewFolder);
+        for (int i = 0; i < 20; i++)
+        {
+            var myVar = InformationModel.MakeVariable("aTag" + i, OpcUa.DataTypes.Int32);
+            Project.Current.Get("Model/Variables").Add(myVar);
+
+        }
+    }
+
+
+}
